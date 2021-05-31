b6d43b3525b93223194efe83a34daf9effe1fc95
Udomsak Chundang <udomsak.chundang@gmail.com>
2021-05-31 09:39:15 +0700

    Added basic authentication and gradle build signkey properties

--- lib/helpers/configure_amplify.dart
+++ lib/helpers/configure_amplify.dart
@@ -1,16 +1,16 @@
 import 'package:amplify_flutter/amplify.dart';
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
-import 'package:amplify_analytics_pinpoint/amplify_analytics_pinpoint.dart';
+// import 'package:amplify_analytics_pinpoint/amplify_analytics_pinpoint.dart';
 
 import '../amplifyconfiguration.dart';
 
 Future<void> configureAmplify() async {
   final auth = AmplifyAuthCognito();
-  final analytics = AmplifyAnalyticsPinpoint();
+  // final analytics = AmplifyAnalyticsPinpoint();
 
   try {
-    Amplify.addPlugins([auth, analytics]);
-
+    // Amplify.addPlugins([auth, analytics]);
+    Amplify.addPlugins([auth]);
     await Amplify.configure(amplifyconfig);
   } on AmplifyAlreadyConfiguredException catch (e) {
     print(e);
--- lib/main.dart
+++ lib/main.dart
@@ -1,75 +1,53 @@
-import 'package:flutter_login/theme.dart';
+// import 'package:flutter_login/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_login/flutter_login.dart';
 
-void main() {
+import 'screens/entry.dart';
+import 'screens/confirm.dart';
+import 'screens/confirm_reset.dart';
+import 'screens/dashboard.dart';
+
+import 'helpers/configure_amplify.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await configureAmplify();
   runApp(MyApp());
 }
 
+
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'AmplifyDemo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'AWS Amplify Demo.'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      onGenerateRoute: (settings) {
+        if (settings.name == '/confirm') {
+          return PageRouteBuilder(
+            pageBuilder: (_, __, ___) =>
+                ConfirmScreen(data: settings.arguments as LoginData),
+            transitionsBuilder: (_, __, ___, child) => child,
+          );
+        }
+        if (settings.name == '/confirm-reset') {
+          return PageRouteBuilder(
+            pageBuilder: (_, __, ___) =>
+                ConfirmResetScreen(data: settings.arguments as LoginData),
+            transitionsBuilder: (_, __, ___, child) => child,
+          );
+        }
+        if (settings.name == '/dashboard') {
+          return PageRouteBuilder(
+            pageBuilder: (_, __, ___) => DashboardScreen(),
+            transitionsBuilder: (_, __, ___, child) => child,
+          );
+        }
+        return MaterialPageRoute(builder: (_) => EntryScreen());
+      },
     );
   }
 }
